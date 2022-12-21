4670ebd94b46ffd5a4f87faec474e6cddb06c07b
tuanna2 <tuanna2@vmodev.com>
2022-12-21 21:35:24 +0700

    Update v2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1 style="color: orange; text-align:center">Tyler Nguyen - V1<h1>');
+  res.send('<h1 style="color: orange; text-align:center">Tyler Nguyen - V2<h1>');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
