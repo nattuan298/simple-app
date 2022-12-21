f1b830a238c13d6f832645e04a864e0e62a97c60
tuanna2 <tuanna2@vmodev.com>
2022-12-21 21:04:46 +0700

    Push to master

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('<h1 style="color: orange; text-align:center">Tyler Nguyen - V1<h1>');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
